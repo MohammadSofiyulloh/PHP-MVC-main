3bca671d94a0b7673ea45345c07a16d0fbb74bb1
Mohammad Sofiyulloh <96521563+MohammadSofiyulloh@users.noreply.github.com>
2021-12-25 20:55:30 +0700

    Delete PHP-MVC-main directory

--- PHP-MVC-main/app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/PHP-MVC-main/public');
-
-//DB
-define('DB_HOST', "localhost");
-define('DB_USER', "root");
-define('DB_PASS', "");
-define('DB_NAME', "mvc_native_php");
